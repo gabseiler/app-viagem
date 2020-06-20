2d24ba9ce7c58ec20cd0ec2662cd4c1d5447c5f6
FabianaDantas <fdantas.santos@outlook.com>
2020-06-20 01:00:32 -0300

    m

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  rapidapikey: '0e3056d71amshd616de54b80603dp1c42c3jsna531b4e8b76c',
+  rapidapikey: 'da3a731818mshd19d692e5f213b3p198466jsn3484bede7fa5',
   cors: 'https://go-far.herokuapp.com/'
 };
