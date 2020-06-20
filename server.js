235af7da2df34492d9f1a896ea53c2ef2ebf0979
FabianaDantas <fdantas.santos@outlook.com>
2020-06-19 23:51:29 -0300

    adicionando header

--- server.js
+++ server.js
@@ -7,6 +7,28 @@ const app = express();
 
  // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://viajando-app.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
  
 app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
